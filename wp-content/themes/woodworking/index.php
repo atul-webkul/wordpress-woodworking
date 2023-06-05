004717c2ec93b333932d318983156129798ca03c
Atul Sharma <atul.sharma892@webkul.in>
2023-06-05 18:59:28 +0530

    added comment

--- wp-content/themes/woodworking/index.php
+++ wp-content/themes/woodworking/index.php
@@ -11,14 +11,19 @@ get_header();
 						
 						<div class="post">
 							<h2 class="title"><a href="#"><?php the_title()?></a></h2>
-							<p class="meta">Posted by <a href="#">Someone</a> on July 10, 2011
-								&nbsp;&bull;&nbsp; <a href="#" class="comments">Comments (64)</a> &nbsp;&bull;&nbsp; <a href="#" class="permalink">Full article</a></p>
+							<p class="meta">Posted by <a href="#"><?php the_author()?></a> on <?php echo get_the_date() ?>
+								&nbsp;&bull;&nbsp; <a href="#" class="comments">Comments (64)</a> &nbsp;&bull;&nbsp; <a href="<?php the_permalink()?>" class="permalink">Full article</a></p>
 							<div class="entry">
-								<p><img src="<?php bloginfo('template_directory')?>/assets/images/img02.jpg" width="143" height="143" alt="" class="alignleft border" />This is <strong>Wood Working </strong>, a free, fully standards-compliant CSS template designed by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a>.  The picture in this template is from <a href="#">PDPhoto.org</a>. This free template is released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attributions</a> license, so you’re pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :)</p>
-								<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in felis. Donec ut ante. In id eros. Suspendisse lacus turpis at sem. Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. </p>
+								<p><img src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id(),'large')[0]?> " width="143" height="143" alt="" class="alignleft border" /> <p>
+									<?php 
+									the_excerpt();
+									?>
+								</p>
 							</div>
 						</div>
-						<?php } ?>
+						<?php
+					 	}
+					  	?>
 
 					</div>
 					<!-- end #content -->
---
+++ wp-content/themes/woodworking/single.php
@@ -0,0 +1,25 @@
+<?php
+get_header();
+the_post();
+?>
+
+<div class="post">
+        <h2 class="title"><a href="#"><?php the_title()?></a></h2>
+        <p class="meta">Posted by <a href="#"><?php the_author(); ?></a> on <?php echo get_the_date() ?>
+            &nbsp;&bull;&nbsp; <a href="#" class="comments">Comments (64)</a> &nbsp;&bull;&nbsp; <a href="<?php the_permalink()?>" class="permalink">Full article</a></p>
+        <div class="entry">
+            <p><img src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id(),'large')[0]?> " width="143" height="143" alt="" class="alignleft border" /> <p>
+                <?php 
+                the_content();
+                ?>
+
+    <?php
+    comments_template( 'comments.php', true );
+    ?>
+        
+            </p>
+        </div>
+    </div>
+<?php
+get_footer();
+?>
